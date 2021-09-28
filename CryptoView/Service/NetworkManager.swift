c387508760dd69b7d4b7d81dad412bdd85031839
Vasilii Pronin <letstalkaboutpork@gmail.com>
2021-09-28 22:19:21 +0300

    NetworkManager setup

---
+++ CryptoView/Service/NetworkManager.swift
@@ -0,0 +1,61 @@
+//
+//  NetworkManager.swift
+//  CryptoView
+//
+//  Created by Василий Пронин on 28.09.2021.
+//
+
+import Foundation
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    func fetchImage(from url: String?, completion: @escaping(Result<Data, NetworkError>) -> Void) {
+        guard let url = URL(string: url ?? "") else {
+            completion(.failure(.invalidURL))
+            return
+        }
+        DispatchQueue.global().async {
+            guard let imageData = try? Data(contentsOf: url) else {
+                completion(.failure(.noData))
+                return
+            }
+            DispatchQueue.main.async {
+                completion(.success(imageData))
+            }
+        }
+
+    }
+    
+    func fetch<T: Decodable>(dataType: T.Type, from url: String, completion: @escaping(Result<T, NetworkError>) -> Void) {
+        guard let url = URL(string: url) else {
+            completion(.failure(.invalidURL))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                completion(.failure(.noData))
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            do {
+                let decoder = JSONDecoder()
+                let type = try decoder.decode(T.self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success(type))
+                }
+            } catch {
+                completion(.failure(.decodingError))
+            }
+        }.resume()
+    }
+}
+
+enum NetworkError: Error {
+    case invalidURL
+    case noData
+    case decodingError
+}
