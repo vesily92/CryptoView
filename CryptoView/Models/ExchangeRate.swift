d5d298eb50a1c47edde3ad7b4bdfb495e83d64bb
Vasilii Pronin <letstalkaboutpork@gmail.com>
2021-09-28 22:18:37 +0300

    Model update

--- CryptoView/Models/ExchangeRate.swift
+++ CryptoView/Models/ExchangeRate.swift
@@ -7,77 +7,106 @@
 
 import Foundation
 
-struct ExchangeRate: Decodable {
-    let fromCurrencyCode: String?
-    let fromCurrencyName: String?
-    let toCurrencyCode: String?
-    let toCurrencyName: String?
-    let exchangeRate: String?
-    let lastRefreshed: String?
-    let timeZone: String?
-    let bidPrice: String?
-    let askPrice: String?
-    
-    enum CodingKeys: String, CodingKey {
-        case fromCurrencyCode = "1. From_Currency Code"
-        case fromCurrencyName = "2. From_Currency Name"
-        case toCurrencyCode = "3. To_Currency Code"
-        case toCurrencyName = "4. To_Currency Name"
-        case exchangeRate = "5. Exchange Rate"
-        case lastRefreshed = "6. Last Refreshed"
-        case timeZone = "7. Time Zone"
-        case bidPrice = "8. Bid Price"
-        case askPrice = "9. Ask Price"
-    }
+enum Link: String {
+    case rates = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey=X6PVSVCEWIEVCUFF"
+    case symbols = "https://finnhub.io/api/v1/crypto/symbol?exchange=binance&token=c56qbp2ad3ibpaikhsrg"
+    case currencies = "https://api.nomics.com/v1/currencies/ticker?key=47b13ceeba5dbb7b6038f2cb14f5044cc2997f7f&ids=BTC,ETH,XRP,USDT,ADA,BNB,SOL,USDC,DOT,DOGE,AVAX,%20LUNA,BUSD,UNI&interval=1d,30d&convert=EUR&per-page=100&page=1"
 }
 
-struct Intraday: Decodable {
-    let metaData: MetaData?
-    let timeSeriesCrypto: TimeSeriesCrypto?
-    
-    enum CodingKeys: String, CodingKey {
-        case metaData = "Meta Data"
-        case timeSeriesCrypto = "Time Series Crypto (5min)"
-    }
-}
-
-struct MetaData: Decodable {
-    let information: String?
-    let digitalCurrencyCode: String?
-    let digitalCurrencyName: String?
-    let marketCode: String?
-    let marketName: String?
-    let lastRefreshed: TimeSeriesCrypto?
-    let interval: String?
-    let outputSize: String?
-    let timeZone: String?
-    
-    enum CodingKeys: String, CodingKey {
-        case information = "1. Information"
-        case digitalCurrencyCode = "2. Digital Currency Code"
-        case digitalCurrencyName = "3. Digital Currency Name"
-        case marketCode = "4. Market Code"
-        case marketName = "5. Market Name"
-        case lastRefreshed = "6. Last Refreshed"
-        case interval = "7. Interval"
-        case outputSize = "8. Output Size"
-        case timeZone = "9. Time Zone"
-    }
-}
-
-struct TimeSeriesCrypto: Decodable {
-    let open: String?
+struct CurrenciesTicker: Decodable {
+    let currency: String?
+    let id: String?
+    let status: String?
+    let price: String?
+    let priceDate: String?
+    let priceTimestamp: String?
+    let symbol: String?
+    let circulatingSupply: String?
+    let maxSupply: String?
+    let name: String?
+    let logoUrl: String?
+    let marketCap: String?
+    let marketCapDominance: String?
+    let transparentMarketCap: String?
+    let numExchanges: String?
+    let numPairs: String?
+    let numPairsUnmapped: String?
+    let firstCandle: String?
+    let firstTrade: String?
+    let firstOrderBook: String?
+    let firstPricedAt: String?
+    let rank: String?
+    let rankDelta: String?
     let high: String?
-    let low: String?
-    let close: String?
-    let volume: Int?
+    let highTimestamp: String?
+    let oneDay: OneDay?
     
     enum CodingKeys: String, CodingKey {
-        case open = "1. open"
-        case high = "2. high"
-        case low = "3. low"
-        case close = "4. close"
-        case volume = "5. volume"
+        case currency = "currency"
+        case id = "id"
+        case status = "status"
+        case price = "price"
+        case priceDate = "price_date"
+        case priceTimestamp = "price_timestamp"
+        case symbol = "symbol"
+        case circulatingSupply = "circulating_supply"
+        case maxSupply = "max_supply"
+        case name = "name"
+        case logoUrl = "logo_url"
+        case marketCap = "market_cap"
+        case marketCapDominance = "market_cap_dominance"
+        case transparentMarketCap = "transparent_market_cap"
+        case numExchanges = "num_exchanges"
+        case numPairs = "num_pairs"
+        case numPairsUnmapped = "num_pairs_unmapped"
+        case firstCandle = "first_candle"
+        case firstTrade = "first_trade"
+        case firstOrderBook = "first_order_book"
+        case firstPricedAt = "first_priced_at"
+        case rank = "rank"
+        case rankDelta = "rank_delta"
+        case high = "high"
+        case highTimestamp = "high_timestamp"
+        case oneDay = "1d"
     }
 }
 
+struct OneDay: Decodable {
+    let priceChange: String?
+    let priceChangePct: String?
+    let volume: String?
+    let volumeChange: String?
+    let volumeChangePct: String?
+    let marketCapChange: String?
+    let marketCapChangePct: String?
+    let transparentMarketCapChange: String?
+    let transparentMarketCapChangePct: String?
+    let volumeTransparency: [VolumeTransparency]?
+    
+    enum CodingKeys: String, CodingKey {
+        case priceChange = "price_change"
+        case priceChangePct = "price_change_pct"
+        case volume = "volume"
+        case volumeChange = "volume_change"
+        case volumeChangePct = "volume_change_pct"
+        case marketCapChange = "market_cap_change"
+        case marketCapChangePct = "market_cap_change_pct"
+        case transparentMarketCapChange = "transparent_market_cap_change"
+        case transparentMarketCapChangePct = "transparent_market_cap_change_pct"
+        case volumeTransparency = "volume_transparency"
+    }
+}
+
+struct VolumeTransparency: Decodable {
+    let grade: String?
+    let volume: String?
+    let volumeChange: String?
+    let volumeChangePct: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case grade = "grade"
+        case volume = "volume"
+        case volumeChange = "volume_change"
+        case volumeChangePct = "volume_change_pct"
+    }
+}
